09e5ea8d04ec678178dc076ff1dfef5213dfbe4e
Dele <deleomololu15@gmail.com>
2024-09-15 21:20:54 -0700

    Update enums.rs

--- enums.rs
+++ enums.rs
@@ -14,10 +14,12 @@ fn main () {
         Office(i32),
 
     }
-    enum Size {
+    /*enum Size {
         Acres(i32),
         SquareFeet(i32),
     }
+    */
+
     enum Location {
         City(String),
         Town(String),
@@ -38,13 +40,6 @@ fn main () {
         DogeCoin(i32),
     }
 
-    let bungalow = Property {
-        property_type: PropertyType::Residential,
-        room: [Rooms::Bedroom(5), Rooms::Bathroom(3)],
-        size: 130680,
-        address: [Location::City(String::from("Big Sur"))],
-    };
-
     fn get_property_price <const T: usize, const Y:usize> (property: Property <T, Y> ) -> String {
         let price_per_square_foot = 200;
         let property_price = price_per_square_foot * property.size;
@@ -52,6 +47,21 @@ fn main () {
         property_price.to_formatted_string(&Locale::en)
     }
 
-    let a:String = get_property_price(bungalow);
-    println!("{a}")
-}
+    let farm_1 = Property {
+        property_type: PropertyType::Industrial,
+        room: [Rooms::Bedroom(5), Rooms::Bathroom(3)],
+        size: 130680,
+        address: [Location::Unincorporated(String::from("Big Sur"))],
+    };
+
+    let headquarters = Property {
+        property_type:PropertyType::Commercial,
+        room: [Rooms::ConferenceRoom(7), Rooms::Office(10), Rooms::Bathroom(4)],
+        size: 800000,
+        address: [Location::City(String::from("San Francisco"))]
+    };
+
+
+    let a: String = get_property_price(farm_1);
+    let b: String = get_property_price(headquarters);
+    println!("The property price of your farm house is ${a} and the property price of your company's headquarters is ${b}")
